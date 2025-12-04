9261c0192389855ed9c7258d35ce941fac63b463
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-04 15:30:55 +0530

    Completed loginform

--- src/main/java/Controller/loginFormController.java
+++ src/main/java/Controller/loginFormController.java
@@ -1,4 +1,21 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
 public class loginFormController {
+    @FXML
+    private CheckBox CboxRememberme;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    public void OnLogin(ActionEvent actionEvent) {
+
+    }
 }
