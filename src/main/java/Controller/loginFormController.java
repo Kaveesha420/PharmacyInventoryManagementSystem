b347dd5c1ae5cbaa8cb510daaa9cac5e3411c9cf
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-05 23:32:58 +0530

    loginForm changes

--- src/main/java/Controller/loginFormController.java
+++ src/main/java/Controller/loginFormController.java
@@ -3,19 +3,23 @@ package Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class loginFormController {
+
     @FXML
     private CheckBox CboxRememberme;
 
     @FXML
-    private TextField txtPassword;
+    private PasswordField txtPassword;
 
     @FXML
     private TextField txtUsername;
 
-    public void OnLogin(ActionEvent actionEvent) {
+    @FXML
+    void OnLogin(ActionEvent event) {
 
     }
+
 }
