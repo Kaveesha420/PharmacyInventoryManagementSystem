9f75ed6c29e7d5456b242820a8cd259e3f1ff7e7
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-06 17:13:35 +0530

    loginForm completed and open dashbord.

---
+++ src/main/java/Controller/DashboardController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class DashboardController {
+
+    @FXML
+    void OnAlerts(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnBilling(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnDashbord(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMedicine(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnSuppliers(MouseEvent event) {
+
+    }
+
+}
--- src/main/java/Controller/loginFormController.java
+++ src/main/java/Controller/loginFormController.java
@@ -2,24 +2,42 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class loginFormController {
 
-    @FXML
-    private CheckBox CboxRememberme;
+    public CheckBox CboxRememberme;
+    public PasswordField txtPassword;
+    public TextField txtUsername;
+    Stage stage = new Stage();
 
     @FXML
-    private PasswordField txtPassword;
+    public void OnLogin(ActionEvent event) {
+        String name = txtUsername.getText();
+        String password = txtPassword.getText();
 
-    @FXML
-    private TextField txtUsername;
-
-    @FXML
-    void OnLogin(ActionEvent event) {
-
+        if ("admin".equals(name) && "1234".equals(password)){
+            try {
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/DashboardForm.fxml"))));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            stage.setTitle("Dashboard");
+            stage.show();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setHeaderText("Invalid User");
+            alert.setContentText("Please Input Valid UserName and Password");
+            alert.showAndWait();
+        }
     }
-
 }
