69396b28b5ae7f9a66917fe7f59e336058bf36f3
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-09 13:54:59 +0530

    Created Supplier Navigation bar

--- src/main/java/Controller/SupplierController.java
+++ src/main/java/Controller/SupplierController.java
@@ -2,11 +2,18 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SupplierController {
+
+    Stage stage = new Stage();
 
     @FXML
     private TableColumn<?, ?> colAddress;
@@ -36,7 +43,13 @@ public class SupplierController {
 
     @FXML
     void OnAlerts(MouseEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/AletsForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Alets");
+        stage.show();
     }
 
     @FXML
@@ -46,12 +59,24 @@ public class SupplierController {
 
     @FXML
     void OnDashbord(MouseEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/DashboardForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Dashboard");
+        stage.show();
     }
 
     @FXML
     void OnMedicine(MouseEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/MedicineForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Medicine");
+        stage.show();
     }
 
     @FXML
