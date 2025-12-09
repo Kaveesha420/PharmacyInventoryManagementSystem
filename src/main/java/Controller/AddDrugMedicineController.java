977c7ab812b2aba4dd36a40345a59c8f7ac3d540
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-09 14:54:14 +0530

    completed ui addnewdrug

--- src/main/java/Controller/AddDrugMedicineController.java
+++ src/main/java/Controller/AddDrugMedicineController.java
@@ -1,4 +1,144 @@
 package Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddDrugMedicineController {
+
+    Stage stage = new Stage();
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnClear;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colBrandName;
+
+    @FXML
+    private TableColumn<?, ?> colCurrentStock;
+
+    @FXML
+    private TableColumn<?, ?> colDosage;
+
+    @FXML
+    private TableColumn<?, ?> colExpireDate;
+
+    @FXML
+    private TableColumn<?, ?> colGenaricName;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private TableView<?> tblMedicine;
+
+    @FXML
+    private JFXTextField txtBrandName;
+
+    @FXML
+    private JFXTextField txtCurrentStock;
+
+    @FXML
+    private JFXTextField txtDosage;
+
+    @FXML
+    private JFXTextField txtExpireDate;
+
+    @FXML
+    private JFXTextField txtGenaricName;
+
+    @FXML
+    private JFXTextField txtUnitPrice;
+
+    @FXML
+    void OnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnUpdate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClear(ActionEvent event) {
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @FXML
+    void OnAlerts(MouseEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/AletsForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Alets");
+        stage.show();
+    }
+
+    @FXML
+    void OnBilling(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnDashbord(MouseEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/DashboardForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Dashboard");
+        stage.show();
+    }
+
+    @FXML
+    void OnMedicine(MouseEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/MedicineForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Medicine");
+        stage.show();
+    }
+
+    @FXML
+    void OnSuppliers(MouseEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/SupplierForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Suppliers");
+        stage.show();
+    }
+
+
+
 }
