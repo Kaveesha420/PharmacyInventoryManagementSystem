f54a8f97a7834e7709677e70860eb2773cf42d24
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-09 14:04:32 +0530

    Created addNewDrug file

--- src/main/java/Controller/MedicineFormController.java
+++ src/main/java/Controller/MedicineFormController.java
@@ -43,7 +43,13 @@ public class MedicineFormController {
 
     @FXML
     void OnAddNewDrug(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/AddDrugMedicine.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("AddNewDrug");
+        stage.show();
     }
 
     @FXML
