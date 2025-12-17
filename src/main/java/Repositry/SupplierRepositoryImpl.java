7cfa24e3316b7fc9781e7af5a8fc64f5e5d1c416
Kaveesha420 <icetkaveesha@gmail.com>
2025-12-17 19:47:53 +0530

    Completed SupplierRepository interface and finished addSupplier methosd

---
+++ src/main/java/Repositry/SupplierRepositoryImpl.java
@@ -0,0 +1,39 @@
+package Repositry;
+
+import Util.HibernateUtil;
+import model.entity.Supplier;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class SupplierRepositoryImpl implements SupplierRepository{
+
+    @Override
+    public void addSuplier(Supplier supplier) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(supplier);
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateSupplier(Supplier supplier) {
+
+    }
+
+    @Override
+    public void deleteSupplier(String id) {
+
+    }
+
+    @Override
+    public List<Supplier> getAllSupplier() {
+        return List.of();
+    }
+}
